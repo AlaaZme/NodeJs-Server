431afd3dcc5a72f28481fd1af41a747041dddc30
Alaa Daoud <Alaa Daoud>
2017-03-25 19:28:19 +0300

    assdasd

--- server/server.js
+++ server/server.js
@@ -1,13 +1,4 @@
-var env = process.env.NODE_ENV ||'development';
-console.log('env ****** ', env);
-if(env === 'development'){
-process.env.PORT = 3000;
-process.env.MONGODB_URI = "mongodb://localhost:27017/users";
-}else if(env ==='test' ){
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = "mongodb://localhost:27017/usersTest";
-}
-
+require('./config/config');
 var express = require('express');
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');

457df800f94b5f8ce83956b8c65713daa33cc1fe
Alaa Daoud <Alaa Daoud>
2017-03-29 11:22:32 +0300

    first

--- server/models/user.js
+++ server/models/user.js
@@ -119,7 +119,7 @@ console.log("in IF unable to fetch user");
     }
     else{
   console.log(User.uname);
-    User.invalidate("uname", "username must be unique");
+ //   User.invalidate("uname", "username must be unique");
           //  done(new Error("username must be unique"));
     }
 

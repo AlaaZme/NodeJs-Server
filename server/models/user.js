7d3d8ae787434028cde83e228667d8dc1f65dd20
Alaa Daoud <Alaa Daoud>
2017-03-29 10:57:13 +0300

    first

--- server/models/user.js
+++ server/models/user.js
@@ -103,7 +103,7 @@ UserSchema.pre('save',function (next){
 
 
     var temp = User.uname;
-user.collection.findOne({User:this.User}, function(err, User) {
+user.collection.findOne({uname:"Ala"}, function(err, User) {
     if(err){
 console.log("in IF unable to fetch user");
     }

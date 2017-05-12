5c2e03b969a4ce42933e99a4a5cd11dfb3274af0
mustafa12332 <abudalumustafa@gmail.com>
2017-05-13 01:01:57 +0300

    fix order

--- server/routes/suggestRoute.js
+++ server/routes/suggestRoute.js
@@ -7,7 +7,7 @@ const Router=express.Router();
 Router.get('/getcomments',(req,res)=>{
     
     const id = mongoose.Types.ObjectId(req.query.id);
-     Products.findById(id).then((product)=>{
+     Products.findById(id).sort({date: 1}).then((product)=>{
         res.send(product.comments)
      })
     

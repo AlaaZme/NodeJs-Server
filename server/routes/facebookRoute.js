bcabfdcce73d85797785ba07f7d7ef2e45375dbe
mustafa12332 <abudalumustafa@gmail.com>
2017-05-15 15:12:06 +0300

    facebook

--- server/routes/facebookRoute.js
+++ server/routes/facebookRoute.js
@@ -4,9 +4,9 @@ const Router=express.Router();
 const mongoose = require('mongoose');
 
 Router.get('/getAll',(req,res)=>{
-  Facebook.find({}).then((facebook)=>{
-        res.send({success:true});
-  })
+//   Facebook.find({}).then((facebook)=>{
+//         res.send({success:true});
+//   })
 });
 // Router.post('/addFacebook',(req,res)=>{
 //     for(face of req.body){

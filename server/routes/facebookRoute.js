e7915d6ef3502cc96263fb2fa83d8051560334bd
mustafa12332 <abudalumustafa@gmail.com>
2017-05-15 16:49:41 +0300

    facebook routes

--- server/routes/facebookRoute.js
+++ server/routes/facebookRoute.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const Products = require('../models/suggestAProduct');
 const Router=express.Router();
 
 Router.get('/getAll',(req,res)=>{
-    res.send({success:true})
+  Products.find({}).then((products)=>{
+        res.send(products);
+  })
 });
+Router.post('/addFacebook',(req,res)=>{
+    res.send(req.body);
+})
 
 module.exports=Router;
